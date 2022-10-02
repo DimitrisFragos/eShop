b8c68aff4b6abba82287871d9b125dfac840f906
Dimitris Fragkos <FragkosD@piraeusbank.gr>
2022-10-02 05:00:39 +0300

    Prevent cross site scripting at config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 	$password = "Asd123!.";
 	$db_name = "eshopdb";
 	
-	$conn = new mysqli("localhost","eShop",'Asd123!.',"eshopdb");
+	$conn = new mysqli("software.security","eShop",'Asd123!.',"eshopdb");
 	if($conn->connect_error){
 		die("Connection Failed!".$conn->connect_error);
 	}
